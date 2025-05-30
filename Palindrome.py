fc4d0809daeedc5dc9c19bc86a2feb45aecd641d
Fahim <alifhossain174@gmail.com>
2025-05-30 23:19:38 +0200

    Updating Code

--- Palindrome.py
+++ Palindrome.py
@@ -1,15 +1,19 @@
 import re
+from math import ceil
 
 def isPalindrome(s: str) -> bool:
 
-    fullText = s.lower().replace(" ","").strip()
-    cleanText = re.sub(r'[^a-zA-Z0-9\s]', '', fullText)
+    fullText = s.replace(" ","").strip()
+    cleanText = re.sub(r'[^a-zA-Z0-9]', '', fullText.lower())
+
     totalLength = len(cleanText) - 1
+    loopIteration = ceil(len(cleanText) / 2)
     finalResult = True
 
-    for char in cleanText:
-        if char != cleanText[totalLength]:
+    for i in range(loopIteration):
+        if cleanText[i] != cleanText[totalLength]:
             finalResult = False
+            break
         totalLength -= 1
 
     return finalResult
